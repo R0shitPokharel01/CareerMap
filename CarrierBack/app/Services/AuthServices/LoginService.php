09424601c457ef93bf27490fe24dfda5e567eeab
Kair0x-009 <roshit.pokharel24@apexcollege.edu.np>
2026-06-13 12:03:31 +0545

    Refactor authentication logic and add RegisterService for improved register handling

--- CarrierBack/app/Services/AuthServices/LoginService.php
+++ CarrierBack/app/Services/AuthServices/LoginService.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Services\AuthServices;
 
-//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class LoginService {
---
+++ CarrierBack/app/Services/AuthServices/RegisterService.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Services\AuthServices;
+
+//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class RegisterService {
+    public function register(array $credentials){
+
+
+
+       if (User::where('email', $credentials['email'])->exists()) {
+            return [
+            'success'=>false,
+            'message'=>'User already exists!'
+        ];
+        }else{
+        $user = User::create([
+            'name' => $credentials['name'],
+            'email' => $credentials['email'],
+            'role' => 'user',
+            'password' => bcrypt($credentials['password'])
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return [
+            'success'=>true,
+            'message'=>'Registration Success',
+            'token'=>$token
+        ];}
+
+
+    }
+}
+
+
+?>
